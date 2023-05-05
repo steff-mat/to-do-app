82e579588f8ca11e75df82a677d06ee2f35f68a7
Stefan Maties <107488671+steff-mat@users.noreply.github.com>
2023-05-05 19:33:02 +0200

    fixed nav menu
    Nav menu will appear once triggered

--- src/design/Nav.js
+++ src/design/Nav.js
@@ -1,16 +1,16 @@
 import CreateElement from '../logic/CreateElement';
 import EventHandler from '../logic/EventHandler';
+import SetClass from '../logic/SetClass';
 
 export default function Nav() {
-  // const content = document.getElementById('content');
-  // const nav = document.createElement('nav');
-  // content.appendChild(nav);
   CreateElement('nav', 'id', 'nav', '#content', '');
+  SetClass('nav', 'class', 'hide-nav');
   //End of Nav set-up
   CreateElement('button', 'id', 'button_toggle_menu', 'nav', '');
   document
     .getElementById('button_toggle_menu')
-    .setAttribute('class', 'hide_btm');
+    // .setAttribute('class', 'show_btm');
+    .setAttribute('class', 'show_btm');
   // CreateElement('button', 'class', 'button_home', 'nav', 'Home');
   CreateElement('button', 'class', 'button_add_task', 'nav', 'Add Task');
   // CreateElement('button', 'class', 'button_remove_task', 'nav', 'Remove Task');
